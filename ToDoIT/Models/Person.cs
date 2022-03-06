ac7ab9f818d2c7f709a0aa501f95690e26626879
MosesYalman <Moses.y@hotmail.com>
2022-03-06 19:49:08 +0100

    Add project files.

---
+++ ToDoIT/Models/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoIT.Models
+{
+    class Person
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+
+        public Person(int id, string firstName, string lastName)
+        {
+            if ((!String.IsNullOrWhiteSpace(firstName)) && (!String.IsNullOrWhiteSpace(lastName)) || !(id < 0))
+            {
+                this.id = id;
+                this.firstName = firstName;
+                this.lastName = lastName;
+            }
+        }
+
+        public int Id
+        {
+            get => id;
+        }
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    firstName = value;
+                }
+            }
+        }
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    lastName = value;
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => $"{firstName} {lastName}";
+        }
+    }
+}
+
---
+++ ToDoIT/Models/ToDo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoIT.Models
+{
+    class ToDo
+    {
+
+        private int id;
+        private string description;
+        private bool done;
+        public Person assignee;
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if ((!String.IsNullOrWhiteSpace(description) || description.Length < 2))
+                {
+                    throw new ArgumentException("Invalid Data found!!!");
+                }
+                description = value;
+            }
+        }
+        public bool Done
+        {
+            get { return done; }
+        }
+
+        public int Id
+        {
+            get => id;
+        }
+
+
+        public ToDo()
+        { }
+        public ToDo(Person assignee)
+        {
+            this.assignee = assignee;
+        }
+
+        public ToDo(int id, string description)
+        {
+            this.id = id;
+            this.description = description;
+        }
+
+        public ToDo(int id, string description, bool done)
+        {
+            this.done = done;
+            this.id = id;
+            this.description = description;
+        }
+
+        public ToDo(int id, string description, bool done, Person person)
+        {
+            this.done = done;
+            this.id = id;
+            this.description = description;
+            this.assignee = person;
+        }
+
+    }
+}
