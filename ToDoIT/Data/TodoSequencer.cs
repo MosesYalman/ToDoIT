358a39fcb095bbcd89f148b1d6d35918de25ee3f
MosesYalman <Moses.y@hotmail.com>
2022-03-06 20:13:37 +0100

    Create PersonSequencer but just different names to suit ToDo instead of Person

---
+++ ToDoIT/Data/TodoSequencer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoIT.Data
+{
+    class TodoSequencer
+    {
+        private static int todoId;
+
+        public static int NextToDoId()
+        {
+            return todoId++;
+
+        }
+
+        public static void Reset()
+        {
+            todoId = 0;
+        }
+
+    }
+}
